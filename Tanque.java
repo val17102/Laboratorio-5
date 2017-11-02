bd7ef5c3c8aa2d412a01558d6513900ef1ba731a
val17102 <32184574+val17102@users.noreply.github.com>
2017-11-02 17:18:21 -0600

    Update Tanque.java

--- Tanque.java
+++ Tanque.java
@@ -5,11 +5,29 @@ public class Tanque{
   protected double cTotal; 
   protected double cActual;
   protected double altura;
-  protected Valvula[] Valvulas;
+  protected Valvula[] valvulas;
 
   public Tanque(){
-  
-  
+  	altura = 30.0;
+  	valvulas = new Valvula[10];
+  }
+
+  public void abrirValvula(int i){
+  	valvulas[i].abrirValvula();
+  	calcularcActual();
+
+  }
+
+  public void cerrarValvula(int i){
+  	valvulas[i].cerrarValvula();
+  	calcularcActual();
+  }
+
+  public void calcularcActual(){
+  		cActual = cTotal;
+  	for(int i = 0; i<=10; i++){
+  		cActual = cActual - valvulas[i].calcularSalida();
+  	}
   }
 
 }
