1471f6f96bbb8591ead89adaafc74058b64e3462
val17102 <32184574+val17102@users.noreply.github.com>
2017-11-02 16:26:49 -0600

    Create Tanque.java

---
+++ Tanque.java
@@ -0,0 +1,15 @@
+public class Tanque{
+  
+  protected String IDNum;
+  protected String tipo;
+  protected double cTotal; 
+  protected double cActual;
+  protected double altura;
+  protected Valvula[] Valvulas;
+
+  public Tanque(){
+  
+  
+  }
+
+}
